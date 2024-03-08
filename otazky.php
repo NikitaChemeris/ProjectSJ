4ca06d934fd2b7ba41bb784254ffee98393333d2
Nikita_IT_2004 <112660966+NikitaChemeris@users.noreply.github.com>
2024-03-08 12:41:12 +0100

    Novy otazky

---
+++ otazky.php
@@ -0,0 +1,17 @@
+<?php
+// Pole otázok
+$otazky = array(
+    "Aké sú vase skúsenostis s PHP?",
+    "Aký je váš obľúbený programovací jazyk?",
+    "Aké sú vaše ciele pre túto stránku?" ,
+    "Ake je najlepšie zviera?",
+);
+// Pole odpovedi
+$odpovede = array(
+    "Mám základné znalosti PHP a skúsenosti, ktoré neviem použit.",
+    "Môj oblübeny programovaci jazyk bude určite PHP. ",
+    "Mojím cieľom je prejsť tento predmet.",
+    "Odpoveď bude vždy kačica.",
+);
+
+?>
